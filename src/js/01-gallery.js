c54a4aea8ea9d27f05bb6f2d8523435af423d177
MykolaS28 <pssolenko@gmail.com>
2023-07-30 23:07:33 +0200

    add 1 2 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryElement = document.querySelector(".gallery");
+const markup = createElement(galleryItems);
 
-console.log(galleryItems);
+function createElement(galleryItems) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`,
+    ""
+  );
+}
+
+galleryElement.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const inputEl = formEl.querySelector('input[name="email"]');
+const textareaEl = formEl.querySelector('textarea[name="message"]');
+
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+formEl.addEventListener('input', throttle(storageFormData, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (inputEl.value === '' || textareaEl.value === '') {
+    return alert('Заповніть всі поля!');
+  }
+
+  console.log(formData);
+
+  formEl.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+function reloadPage() {
+  inputEl.value = formData.email || '';
+  textareaEl.value = formData.message || '';
+}
